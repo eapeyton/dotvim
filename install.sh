b87192adb6891bbff0f096e4274c28ee0702580d
Eric Peyton <ea.peyton@gmail.com>
2013-08-26 23:07:45 -0400

    Adding install script

---
+++ install.sh
@@ -0,0 +1,18 @@
+directory="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+echo "Found repository directory as $directory"
+
+cd ~
+
+echo "Linking .vimrc..."
+ln -sf $directory/vimrc ~/.vimrc
+
+echo "Linking .gvimrc..."
+ln -sf $directory/gvimrc ~/.gvimrc
+
+echo "Updating git submodule..."
+cd $directory
+git submodule init
+git submodule update
+
+echo "Done"
+
